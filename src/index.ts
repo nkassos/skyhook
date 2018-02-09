9d57b5bdd95b3e5c0abadc9d2d64b15d0a77aa01
noahk <noah.kassos@openmarket.com>
2018-02-09 08:58:32 -0800

    A couple mods to support testing in external modules.  Should be replaced with a more elegant system.

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,10 @@ import Service from './loaders/AnnotationLoader/annotations/Service';
 import Inject from './loaders/AnnotationLoader/annotations/Inject';
 import Qualifier from './loaders/AnnotationLoader/annotations/Qualifier';
 import Factory from './loaders/AnnotationLoader/annotations/Factory';
-import PostConstruct from './loaders/AnnotationLoader/annotations/PostConstruct'
+import PostConstruct from './loaders/AnnotationLoader/annotations/PostConstruct';
 
-export { Skyhook, Context, LoadingDock, AnnotationLoader, Service, Inject, Qualifier, Factory, PostConstruct };
+// This is being provided currently as a hack for skyhook-express.  This should be replaced with a proper system for extension
+import ServiceKey from './loaders/AnnotationLoader/symbols/ServiceKey';
+
+export { Skyhook, Context, LoadingDock, AnnotationLoader, Service, Inject, Qualifier, Factory, PostConstruct, ServiceKey };
 export default Skyhook;
